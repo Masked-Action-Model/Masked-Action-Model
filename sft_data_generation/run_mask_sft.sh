48efad6a8c9911446bb0149f9c068e7afcfea174
yanbangli <liyanbang0077@gmail.com>
2025-08-04 16:56:51 +0800

    update

--- sft_data_generation/run_mask_sft.sh
+++ sft_data_generation/run_mask_sft.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-INPUT="/data1/tangjielong/VLA_project/se3_raw/StackPyramid-v1/motionplanning/20250716_205039_mask.h5"
+#INPUT="/data1/tangjielong/VLA_project/se3_raw/StackPyramid-v1/motionplanning/20250716_205039_mask.h5"
+INPUT="../Data/arlen_data/demos/PickCube-v1/motionplanning/20250716_184350_mask.h5"
 OUTPUT="./test_local_planner_mask.h5"
 MASK_TYPE="local_planner"
-RETAIN_RATIO=0.2
+RETAIN_RATIO=0.9
 SIZE=5
 
 python mask_for_sft.py \
